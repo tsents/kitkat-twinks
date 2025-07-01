5f54406258037f7177e92b9e1b69cd1540b06046
tsents <tsents.sagie@gmail.com>
2025-07-01 17:00:35 +0300

    Add basic sync to the main function to allow checking of one runnig program.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include "key_manager.h"
+#include <iostream>
 
 const LPCTSTR MESSAGE = "MANAGEMENT PROGRAM IS UP";
 const LPCTSTR TITLE = "Managment";
 
+const LPCSTR MUTEX_NAME = "Global\\Engineer-Mutex";
+
+// Key used to make us run on startup
+const LPCSTR TARGET_KEY = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
 
 int main() {
+    HANDLE singleProgramMutex = CreateMutex(NULL, FALSE, MUTEX_NAME);
+	if (singleProgramMutex == NULL) {
+		std::cout << "Got error in Mutex creation " << GetLastError() << std::endl;
+	}
+	DWORD waitResult = WaitForSingleObject(singleProgramMutex, 0);
+	if (waitResult != WAIT_OBJECT_0) {
+		std::cout << "Got bad result " << waitResult << std::endl;
+		std::cout << GetLastError() << std::endl;
+		std::cout << "(I THINK) Program is allready running" << std::endl;
+		return 0;
+	}
+
 	MessageBox(NULL, MESSAGE, TITLE, MB_OK | MB_ICONINFORMATION);
 
-	KeyContext *autostartKey = new KeyContext(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+	KeyContext *autostartKey = new KeyContext(HKEY_CURRENT_USER, TARGET_KEY);
+
 
 	//std::cout << autostartKey->getKeyValue("ENGINEER") << std::endl;
 
