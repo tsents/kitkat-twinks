ac5eef14f88c42f676c475b751fa1e147b2a981e
tsents <tsents.sagie@gmail.com>
2025-07-01 11:55:45 +0300

    Sucessfull test of obtaining the registry key.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,33 @@
-#include <processthreadsapi.h> //GetCurrentProcess().
-//#include <winuser.h> //MessageBox()
 #include <windows.h>
-
+#include <stdio.h> //For printf.
+#include <stdlib.h>
+#include <ntdef.h>
 
 const LPCTSTR MESSAGE = "MANAGEMENT PROGRAM IS UP";
 const LPCTSTR TITLE = "Managment";
 
-/*
-    int msgboxID = MessageBoxA(
-        NULL,
-        (LPCWSTR)L"Resource not available\nDo you want to try again?",
-        (LPCWSTR)L"Account Details",
-        MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
-    );
-
-    switch (msgboxID)
-    {
-    case IDCANCEL:
-        // TODO: add code
-        break;
-    case IDTRYAGAIN:
-        // TODO: add code
-        break;
-    case IDCONTINUE:
-        // TODO: add code
-        break;
-    }
-*/
 
 int main() {
 	MessageBox(NULL, MESSAGE, TITLE, MB_OK | MB_ICONINFORMATION);
+
+	HKEY autorunKey = NULL;
+	LSTATUS lResult = RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_ALL_ACCESS, &autorunKey);
+	if (lResult != ERROR_SUCCESS) {
+		if (lResult == ERROR_FILE_NOT_FOUND) {
+			printf("Key not found.\n");
+			return TRUE;
+		}
+		else {
+			printf("Error opening key.\n");
+			return FALSE;
+		}
+	}
+	char* engineerKeyData = (char*)malloc(100);
+	DWORD length = 100;
+	RegGetValueA(autorunKey, NULL, "ENGINEER", RRF_RT_REG_SZ, NULL, engineerKeyData, &length);
+	printf("%s\n", engineerKeyData);
+	
+
+	
 	return 0;
 }
