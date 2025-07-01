b2f51f2013b2020acafa809f50ed06b8e49064ad
tsents <tsents.sagie@gmail.com>
2025-07-01 16:20:46 +0300

    Add proper docs and better naming for varibles.

--- key_manager.cpp
+++ key_manager.cpp
@@ -20,27 +20,28 @@ KeyContext::~KeyContext() {
 	RegCloseKey(m_keyHandle);
 }
 
-BYTE* KeyContext::getKeyValue(LPCSTR value) {
+BYTE* KeyContext::getKeyValue(LPCSTR field) {
 	DWORD length = 0;
-	LSTATUS lResult = RegGetValueA(m_keyHandle, NULL, value, RRF_RT_REG_SZ, NULL, NULL, &length); //To get needed length
+	LSTATUS lResult = RegGetValueA(m_keyHandle, NULL, field, RRF_RT_REG_SZ, NULL, NULL, &length); //To get needed length
 	if (lResult != ERROR_SUCCESS && lResult != ERROR_MORE_DATA) {
 		std::cout << "Couldn't get key length" << std::endl;
 		return NULL;
 	}	
 	BYTE* keyData = new BYTE[length];
-	lResult = RegGetValueA(m_keyHandle, NULL, value, RRF_RT_REG_SZ, NULL, keyData, &length);
+	lResult = RegGetValueA(m_keyHandle, NULL, field, RRF_RT_REG_SZ, NULL, keyData, &length);
 	if (lResult != ERROR_SUCCESS) {
 		std::cout << "Failed loading key after length check " << length << std::endl;
 		return NULL;
 	}
-	std::cout << "Obtained key value: " << keyData << std::endl;
+	std::cout << "Obtained key field: " << keyData << std::endl;
 	return keyData;
 }
 
-int KeyContext::setKeyValue(const BYTE* value, LPCSTR valueName) {
-	LSTATUS lResult = RegSetValueExA(m_keyHandle, valueName, FALSE, REG_SZ, value, strlen((const char*)value) + 1); 
+int KeyContext::setKeyValue(const BYTE* value, LPCSTR field) {
+	LSTATUS lResult = RegSetValueExA(m_keyHandle, field, FALSE, REG_SZ, value, strlen((const char*)value) + 1); 
 	if (lResult != ERROR_SUCCESS) {
-		std::cout << "Error encountered in setKeyValue " << value << " " << valueName << " " << lResult << std::endl;
+		std::cout << "Error encountered in setKeyValue " << value << " " << field << " " << lResult << std::endl;
+		return FALSE;
 	}
 	RegCloseKey(m_keyHandle);
 	return TRUE;
--- key_manager.h
+++ key_manager.h
@@ -2,14 +2,32 @@
 #include <windows.h>
 #include <ntdef.h>
 
+
+/*
+ * Implements a simplified context object for reading and setting registry keys.
+ * the class assumes that the key value is RRF_RT_REG_SZ (string) and that names
+ * are given in ASCI format (uses A functions instead of W).
+ */ 
 class KeyContext {
 public:
+	/*
+ 	 * Opens a handle to the key. saving into m_keyHandle.
+ 	 */
 	KeyContext(HKEY rootKey, LPCSTR subKey);
+	// Closes the m_keyHandle.
 	~KeyContext();	
 
-	BYTE* getKeyValue(LPCSTR value);
+	/*
+ 	 * Gets the value stored in a key, based on specific field.
+ 	 */
+	BYTE* getKeyValue(LPCSTR field);
 
-	int setKeyValue(const BYTE* value, LPCSTR valueName);
+	/*
+	 * Sets the value of the field given to be <value> overriding any old data.
+	 *
+	 * return [OUT] TRUE or FALSE based if the action succeeded 
+	 */
+	int setKeyValue(const BYTE* value, LPCSTR field);
 private:
 	HKEY m_keyHandle;
 };
