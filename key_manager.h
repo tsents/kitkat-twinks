59ba27f58f95c40147532521f2d4a76c70738275
tsents <tsents.sagie@gmail.com>
2025-07-01 18:10:09 +0300

    Change type to be const. as it doesnt change in copying.

--- key_manager.h
+++ key_manager.h
@@ -27,7 +27,7 @@ public:
 	 *
 	 * return [OUT] TRUE or FALSE based if the action succeeded 
 	 */
-	int setKeyValue(LPSTR value, LPCSTR field);
+	int setKeyValue(LPCSTR value, LPCSTR field);
 private:
 	HKEY m_keyHandle;
 };
