55058955cc48dd05b3b6a42396cf6384d53648ae
tsents <tsents.sagie@gmail.com>
2025-07-02 13:06:14 +0300

    Fix formatting, and add lReslt to error print.

--- RegistryKey.cpp
+++ RegistryKey.cpp
@@ -2,49 +2,48 @@
 #include <iostream>
 #include <wchar.h> //for wcslen
 
-
 RegistryKey::RegistryKey(HKEY rootKey, LPCSTR subKey) : m_keyHandle(NULL) {
-	LSTATUS lResult = RegOpenKeyExA(rootKey, subKey, 0, KEY_ALL_ACCESS, &m_keyHandle);
-	if (lResult != ERROR_SUCCESS) {
-		if (lResult == ERROR_FILE_NOT_FOUND) {
-			std::cout << "Key not found." << std::endl;
-			return;
-		}
-		else {
-			std::cout << "Error opening key." << std::endl;
-			return;
-		}
-	}
+    LSTATUS lResult = RegOpenKeyExA(rootKey, subKey, 0, KEY_ALL_ACCESS, &m_keyHandle);
+    if (lResult != ERROR_SUCCESS) {
+        if (lResult == ERROR_FILE_NOT_FOUND) {
+            std::cout << "Key not found." << std::endl;
+            return;
+        } else {
+            std::cout << "Error opening key " << lResult << std::endl;
+            return;
+        }
+    }
 }
 
 RegistryKey::~RegistryKey() {
-	RegCloseKey(m_keyHandle);
+    RegCloseKey(m_keyHandle);
 }
 
 LPWSTR RegistryKey::getKeyValue(LPCWSTR field) {
-	DWORD length = 0;
-	LSTATUS lResult = RegGetValueW(m_keyHandle, NULL, field, RRF_RT_REG_SZ, NULL, NULL, &length); //To get needed length
-	if (lResult != ERROR_SUCCESS && lResult != ERROR_MORE_DATA) {
-		std::cout << "Couldn't get key length" << std::endl;
-		return NULL;
-	}	
-	LPWSTR keyData = new WCHAR[length];
-	lResult = RegGetValueW(m_keyHandle, NULL, field, RRF_RT_REG_SZ, NULL, keyData, &length);
-	if (lResult != ERROR_SUCCESS) {
-		std::cout << "Failed loading key after length check " << length << std::endl;
-		return NULL;
-	}
-	return keyData;
+    DWORD length = 0;
+    LSTATUS lResult = RegGetValueW(m_keyHandle, NULL, field, RRF_RT_REG_SZ, NULL, NULL, &length); // To get needed
+                                                                                                  // length
+    if (lResult != ERROR_SUCCESS && lResult != ERROR_MORE_DATA) {
+        std::cout << "Couldn't get key length" << std::endl;
+        return NULL;
+    }
+    LPWSTR keyData = new WCHAR[length];
+    lResult = RegGetValueW(m_keyHandle, NULL, field, RRF_RT_REG_SZ, NULL, keyData, &length);
+    if (lResult != ERROR_SUCCESS) {
+        std::cout << "Failed loading key after length check " << length << std::endl;
+        return NULL;
+    }
+    return keyData;
 }
 
 BOOL RegistryKey::setKeyValue(LPCWSTR value, LPCWSTR field) {
-	//uses BYTE* because other key field types are allowed. not only strings.
-	const BYTE* byteData = reinterpret_cast<const BYTE*>(value);
-	const size_t dataSize = (wcslen(value) + 1) * sizeof(wchar_t);
-	LSTATUS lResult = RegSetValueExW(m_keyHandle, field, FALSE, REG_SZ, byteData, dataSize); 
-	if (lResult != ERROR_SUCCESS) {
-		std::cout << "Error encountered in setKeyValue " << value << " " << field << " " << lResult << std::endl;
-		return FALSE;
-	}
-	return TRUE;
+    // uses BYTE* because other key field types are allowed. not only strings.
+    const BYTE* byteData = reinterpret_cast<const BYTE*>(value);
+    const size_t dataSize = (wcslen(value) + 1) * sizeof(wchar_t);
+    LSTATUS lResult = RegSetValueExW(m_keyHandle, field, FALSE, REG_SZ, byteData, dataSize);
+    if (lResult != ERROR_SUCCESS) {
+        std::cout << "Error encountered in setKeyValue " << value << " " << field << " " << lResult << std::endl;
+        return FALSE;
+    }
+    return TRUE;
 }
--- RegistryKey.h
+++ RegistryKey.h
@@ -1,43 +1,43 @@
 #pragma once
-#include <windows.h>
 #include <ntdef.h>
-
+#include <windows.h>
 
 /*
  * Implements a simplified context object for reading and setting registry keys.
  * the class assumes that the key value is RRF_RT_REG_SZ (string) and that names
  * are given in ASCI format (uses A functions instead of W).
- */ 
+ */
 class RegistryKey {
 public:
-	/*
- 	 * Opens a handle to the key. saving into m_keyHandle.
- 	 * Uses the same paramater scheme as RegGetValueA.
- 	 *
- 	 * rootKey [IN] Handle to the key to search under. 
- 	 * subKey  [IN] Path to subkey to search. use NULL to ignore.
- 	 */
-	RegistryKey(HKEY rootKey, LPCSTR subKey);
-	// Closes the m_keyHandle.
-	~RegistryKey();	
+    /*
+     * Opens a handle to the key. saving into m_keyHandle.
+     * Uses the same paramater scheme as RegGetValueA.
+     *
+     * rootKey [IN] Handle to the key to search under.
+     * subKey  [IN] Path to subkey to search. use NULL to ignore.
+     */
+    RegistryKey(HKEY rootKey, LPCSTR subKey);
+    // Closes the m_keyHandle.
+    ~RegistryKey();
 
-	/*
- 	 * Gets the value stored in a key, based on specific field.
- 	 * The returned string is allocated on the heap. make sure to delete it.
- 	 *
- 	 * field  [IN]  The filed under the key to get.
- 	 * return [OUT] A buffer of cost char. this buffer is allocated on the heap.
- 	 */
-	LPWSTR getKeyValue(LPCWSTR field);
+    /*
+     * Gets the value stored in a key, based on specific field.
+     * The returned string is allocated on the heap. make sure to delete it.
+     *
+     * field  [IN]  The filed under the key to get.
+     * return [OUT] A buffer of cost char. this buffer is allocated on the heap.
+     */
+    LPWSTR getKeyValue(LPCWSTR field);
 
-	/*
-	 * Sets the value of the field given to be <value> overriding any old data.
-	 *
-	 * value  [IN]  The value to put inside the key.
-	 * entry  [IN]  The entry under the key to set.
-	 * return [OUT] TRUE or FALSE based if the action succeeded.
-	 */
-	BOOL setKeyValue(LPCWSTR value, LPCWSTR field);
+    /*
+     * Sets the value of the field given to be <value> overriding any old data.
+     *
+     * value  [IN]  The value to put inside the key.
+     * entry  [IN]  The entry under the key to set.
+     * return [OUT] TRUE or FALSE based if the action succeeded.
+     */
+    BOOL setKeyValue(LPCWSTR value, LPCWSTR field);
+
 private:
-	HKEY m_keyHandle;
+    HKEY m_keyHandle;
 };
